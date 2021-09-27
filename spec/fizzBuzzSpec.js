39606f878688639dce7f81bed013f967655048f4
EMDevelop <edwardmartin.inbox@gmail.com>
2021-09-27 19:36:35 +0100

    is div by 5 added

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -11,6 +11,24 @@ describe("FizzBuzz", function () {
     });
   });
 
+  describe("number is div by 3", function () {
+    it("is Fizz", function () {
+      expect(fizzBuzz.start(6)).toBe("Fizz");
+    });
+  });
+
+  describe("number is div by 5", function () {
+    it("is Buzz", function () {
+      expect(fizzBuzz.start(5)).toBe("Buzz");
+    });
+  });
+
+  describe("number is div by 5", function () {
+    it("is Buzz", function () {
+      expect(fizzBuzz.start(10)).toBe("Buzz");
+    });
+  });
+
   // describe("div by 5", function () {
   //   it("is Buzz", function () {
   //     expect(fizzBuzz.isDivisibleByFive(5)).toBe("Buzz");
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,9 +1,14 @@
 class FizzBuzz {
   start(num) {
-    if (this.isDivisibleByThree(num)) return "Fizz";
+    if (this._isDivisibleByThree(num)) return "Fizz";
+    if (this.__isDivisibleByFive(num)) return "Buzz";
   }
 
-  isDivisibleByThree(number) {
+  __isDivisibleByFive(number) {
+    return this._isDivisibleBy(number, 5);
+  }
+
+  _isDivisibleByThree(number) {
     return this._isDivisibleBy(number, 3);
   }
 
